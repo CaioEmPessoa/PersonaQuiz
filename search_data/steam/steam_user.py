484076e922336c1ed27ba226f9e2d64187865a0c
Caio Polonio <127911795+CaioEmPessoa@users.noreply.github.com>
2023-09-30 00:23:50 -0300

    Added "waitress" to deploy project

--- search_data/steam/steam_user.py
+++ search_data/steam/steam_user.py
@@ -388,7 +388,9 @@ def header_apply(response):
     return response
 
 if __name__ == "__main__":
+    from waitress import serve
     Start = SteamMaker()
     app.add_url_rule
-    app.run(host="0.0.0.0" port=5000)
+    serve(app, host="0.0.0.0", port=5000)
+
 
