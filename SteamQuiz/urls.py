e69399dfd1e400cc3bdfceb14d4084ac065a75c3
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-04-27 13:55:33 -0300

    Added new pages with dinamic style changes.

--- SteamQuiz/urls.py
+++ SteamQuiz/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("read_quiz/<str:id>", views.read, name="read"),
+    path("create_quiz/", views.create, name="create")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SteamQuiz/views.py
+++ SteamQuiz/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+STYLE_PATH = "css/steam_style.css"
+ICON_PATH = "img/steam_logo.png"
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    api_request = "steam"
+    return render(request, "index.html", 
+                  {
+                      "style_path":STYLE_PATH,
+                      "icon_path":ICON_PATH
+                  })
+
+def read(request, id="none"):
+    return render(request, "read_quiz.html",
+                  {
+                      "style_path":STYLE_PATH,
+                      "icon_path":ICON_PATH
+                  })
+
+def create(request):
+    return render(request, "create_quiz.html",
+                  {
+                      "style_path": STYLE_PATH,
+                      "icon_path":ICON_PATH
+                  })
