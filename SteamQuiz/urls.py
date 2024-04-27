98d384d1f72c2c8b8f1523ac305ce7417fe8aa5a
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-04-27 15:28:24 -0300

    Added url to empty id

--- SteamQuiz/urls.py
+++ SteamQuiz/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("read_quiz/<str:id>", views.read, name="read"),
+    path("read_quiz/", views.read, name="read"),
+    path("read_quiz/<str:id>/", views.read, name="read"),
     path("create_quiz/", views.create, name="create")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
