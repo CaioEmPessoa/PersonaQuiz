f5add907b551abadb113814a937400555d1173a6
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-04-27 13:55:03 -0300

    added static dirs and urls

--- PersonaQuiz/settings.py
+++ PersonaQuiz/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-rpeo&dss-f_p9&x3!4iidd=tf@+lf1exu7v5t+4477livp7cbt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    "static",
+   BASE_DIR / 'static/',
+)
 
 STATIC_URL = 'static/'
 
--- SteamQuiz/urls.py
+++ SteamQuiz/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="home")
-]
+    path("", views.home, name="home"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
