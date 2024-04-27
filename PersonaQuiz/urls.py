d06fb59422e42bca852ff0cd83cc4dca212bfab6
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-04-27 14:59:04 -0300

    Added a "main page" app.

--- PersonaQuiz/urls.py
+++ PersonaQuiz/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('SteamQuiz.urls'))
+    path('', include('MainPage.urls')),
+    path('SteamQuiz/', include('SteamQuiz.urls'))
 ]
