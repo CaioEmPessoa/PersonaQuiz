7197fec6d17053cb69218080a9c137cb1378e636
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-05-03 20:26:08 -0300

    changed statuc url to match render's

--- PersonaQuiz/settings.py
+++ PersonaQuiz/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = BASE_DIR / 'static_root/'
 
-STATIC_URL = '/opt/render/project/src/static_root'
+STATIC_URL = '/opt/render/project/src/static_root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
