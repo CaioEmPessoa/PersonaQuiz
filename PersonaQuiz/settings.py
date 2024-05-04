5ca8c20366af6c5cd3eedc4e5227dafbca882e61
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-05-04 10:58:20 -0300

    Added settings to match render static file handeling

--- PersonaQuiz/settings.py
+++ PersonaQuiz/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,9 +126,12 @@ STATICFILES_DIRS = (
     "static",
    BASE_DIR / 'static_files/',
 )
-STATIC_ROOT = BASE_DIR / 'static_root/'
+# STATIC_ROOT = BASE_DIR / 'static_root/'
 
-STATIC_URL = '/opt/render/project/src/static_root/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
