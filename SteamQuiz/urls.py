0ce19d086222d1f0edcb4de820d3d118115ac891
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-05-17 13:44:07 -0300

    removed read_quiz pages mentions

--- SteamQuiz/urls.py
+++ SteamQuiz/urls.py
@@ -4,12 +4,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # frontend
     path("", views.home, name="home"),
-    path("read_quiz/", views.read, name="read"),
-    path("read_quiz/<str:id>/", views.read, name="read"),
     path("create_quiz/", views.create, name="create"),
     path("quiz/", views.quiz, name="quiz"),
     
+    # backend
     path("api/test/", views.api_test, name="api_test"),
     path("api/create/<username>/<ammount>", views.api_create, name="api_create"), # TODO: switch / with param
     path("api/read/<quiz_id>/", views.api_read, name="api_read")
