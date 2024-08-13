3ebd286c943cf24b57558b492b22d59559f1b8d2
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-08-13 14:41:24 -0300

    Added question functions.

--- LastFM/src/last_user.py
+++ LastFM/src/last_user.py
@@ -1,5 +1,6 @@
 import json
 import pylast
+import random
 
 from decouple import config
 
@@ -40,12 +41,6 @@ class UserInfo():
         for album in user.get_top_albums()[:8]:
             albuns_list.append(pylast.Album.get_name(album.item))
 
-#       Maybe do a last played track later.
-#        for track in user.get_recent_tracks()[:5]:
-#            recent_tracks.append(pylast.PlayedTrack.get_name(track.item))
-#
-
-        # Coleta todas informações do usuario 
 
         self.info_dict = {
             "nome":username,
@@ -62,6 +57,29 @@ class UserInfo():
 
         print(data)
 
+    def qstn_track(self):
+        question = f"Qual a música mais ouvida de {self.info_dict['name']}? "
+        options = self.info_dict["tracks"][4]
+        answer = options[0]
+        options = random.shuffle(self.info_dict["tracks"][4])
+
+        return question, options, answer
+    
+    def qstn_track(self):
+        question = f"Qual o artista mais ouvido de {self.info_dict['name']}? "
+        options = self.info_dict["artists"][4]
+        answer = options[0]
+        options = random.shuffle(self.info_dict["artists"][4])
+
+        return question, options, answer
+    
+    def qstn_track(self):
+        question = f"Qual o álbum mais ouvido de {self.info_dict['name']}? "
+        options = self.info_dict["albuns"][4]
+        answer = options[0]
+        options = random.shuffle(self.info_dict["albuns"][4])
+
+        return question, options, answer
 
     def __init__(self, username):
 
