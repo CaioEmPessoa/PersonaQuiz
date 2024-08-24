9eeba2b36e7e202f83368d5e1603f40bc09a9363
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-08-24 16:34:07 -0300

    Added function to actually make all questions in lastfm

--- LastFM/src/last_user.py
+++ LastFM/src/last_user.py
@@ -11,10 +11,12 @@ class UserInfo():
 
         # Log into lasfm API
         API_KEY = config("LASTFM_API_KEY")
+        self.DEBUG = config("DEBUG")
 
         self.USERNAME = username
 
         self.api = LastfmApi(username, API_KEY)
+
         
         self.get_user_info()
 
@@ -54,14 +56,65 @@ class UserInfo():
 
         print("Informações coletadas!")
 
-        print(data)
+        if self.DEBUG:
+            print(data)
+
+        self.qstn_dict["status"] = "Got Info."
+
+    def make_qstn(self, ammount):
+
+        # Lista de todas funções que fazem as perguntas
+        questions_list = [
+            self.qstn_album, self.qstn_artist, self.qstn_track
+            ]
+        
+        if ammount > len(questions_list):
+            ammount = len(questions_list)
+
+        # A list with the itens on the list
+        can_choose = list(range(len(questions_list)))
+
+        for question in range(ammount):
+            # chooses a random value in can_choose to use it as a index for the questions_list
+            choosed = random.choice(can_choose)
+
+            # remove the already choosed item of the list
+            can_choose.remove(choosed)
+
+            # Opens the function and gets the returned values
+            question, options, answer = questions_list[choosed]()
+
+            # Shuffles the options of the quiz
+            random.shuffle(options)
+
+            # add a number to the questions made
+            self.qstn_dict["qstn_count"] += 1
+
+            # make a dict about the question being made
+            question_info = {
+                "Question " + str(self.qstn_dict["qstn_count"]): {
+                    "question": question,
+                    "options": options,
+                    "answer": answer,
+                },
+            }
+
+            # updates the main dict
+            self.qstn_dict.update(question_info)
+
+            # prints and returns the result
+            if self.DEBUG:
+                print("Questão feita!")
+                print(json.dumps(self.qstn_dict, indent=4))
+
+        # self.save_quiz()
+        self.qstn_dict["status"] = "Quiz Criado!"
+        return self.qstn_dict
 
     def qstn_track(self):
         question = f"Qual a música mais ouvida de {self.info_dict['name']}? "
         options = self.info_dict["tracks"]
         answer = options[0]
-        
-        random.shuffle(self.info_dict["tracks"])
 
         return question, options, answer
     
@@ -69,8 +122,6 @@ class UserInfo():
         question = f"Qual o artista mais ouvido de {self.info_dict['name']}? "
         options = self.info_dict["artists"]
         answer = options[0]
-        
-        random.shuffle(self.info_dict["artists"])
 
         return question, options, answer
     
@@ -79,12 +130,11 @@ class UserInfo():
         options = self.info_dict["albuns"]
         answer = options[0]
 
-        random.shuffle(options)
-
         return question, options, answer
+
 
 
 
 info = UserInfo("CaioEmPessoa")
 
-print(info.qstn_album())
+info.make_qstn(10)
