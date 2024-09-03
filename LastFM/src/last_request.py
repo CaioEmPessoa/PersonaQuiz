6d44b5cb951778dbb0bfe724f0fac9f0cb7feeb1
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-09-03 19:56:42 -0300

    Added request for artists stats

--- LastFM/src/last_request.py
+++ LastFM/src/last_request.py
@@ -40,24 +40,27 @@ class LastfmApi():
 
         self.DEBUG = config("DEBUG")
 
-    def request(self, type, page=1,
-                limit=10, period="7day"):
+    def request(self, type, artist=False, page=1, limit=10, period="overall"):
 
         type = self.TYPE_DICT[type]
 
-        params={"method": f"user.{type["method"]}",
-                "format":"json",
-                "user":self.NAME,
+        params={"format":"json",
                 "limit":limit,
                 "period":period,
                 "page": page,
                 "api_key":self.API_KEY}
 
+        if artist != False:
+            params["artist"] = artist
+            params["method"] = f"artist.{type["method"]}"
+        else:
+            params["user"] = self.NAME
+            params["method"] = f"user.{type["method"]}"
 
         return requests.get("https://ws.audioscrobbler.com/2.0/", params=params)
 
     @lru_cache
-    def topstats(self, type, limit=10, period="overall", full=False):
+    def topstats(self, type, artist=False, limit=10, period="overall", full=False):
         
         if self.DEBUG:
             print("looking for top", type)
@@ -65,13 +68,12 @@ class LastfmApi():
         name = self.TYPE_DICT[type]["name"]
         single = self.TYPE_DICT[type]["single"]
 
-        r = self.request(type=type, limit=limit, period=period)
+        r = self.request(type=type, limit=limit, period=period, artist=artist)
 
         if r.json()[name]["@attr"]["total"] == "0":
             return []
 
         stat_list = r.json()[name][single]
-
 
         stat_list_org = [stat["name"] for stat in stat_list]
 
@@ -81,14 +83,14 @@ class LastfmApi():
             return stat_list
 
     @lru_cache
-    def laststats(self, type, limit=10, page=1, period="overall"):
+    def laststats(self, type, limit=10, page=1, period="overall", artist=False):
         if self.DEBUG:
             print("looking for last", type)
 
         name = self.TYPE_DICT[type]["name"]
         single = self.TYPE_DICT[type]["single"]
 
-        first_r = self.request(type=type, limit=limit, page=page, period=period)
+        first_r = self.request(type=type, limit=limit, page=page, period=period, artist=artist)
 
         if first_r.json()[name]["@attr"]["total"] == "0":
             return []
@@ -96,7 +98,7 @@ class LastfmApi():
         page = first_r.json()[name]["@attr"]["totalPages"] # go to last page
         last_limit = first_r.json()[name]["@attr"]["perPage"] # get the limit of the page to get the last item in it
 
-        last_r = self.request(type=type, limit=last_limit, page=page, period=period)
+        last_r = self.request(type=type, limit=last_limit, page=page, period=period, artist=artist)
 
         stat_list = last_r.json()[name][single]
 
@@ -104,10 +106,6 @@ class LastfmApi():
         stat_list_org[limit:] # cuts the list to the limit requested
 
         return stat_list_org
-
-    def artiststats(self, name):
-        # TODO: request and retrieve data from specific artist, like top tracks/albuns etc...
-        pass
     
     def test_user(self):
         r = self.request("track").json()
@@ -122,5 +120,5 @@ if __name__ == "__main__":
     API_KEY = config("LASTFM_API_KEY")
     api = LastfmApi("caioempessoa", API_KEY)
 
-    re = api.laststats("recent", limit=10)
+    re = api.topstats("track", limit=10, artist="gorillaz")
     print(re)
